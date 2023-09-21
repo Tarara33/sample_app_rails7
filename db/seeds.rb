f1f4091c2422d361e21ae19f89bef925945741e2
SARINA <your.email@example.com>
2023-09-21 17:44:48 +0000

    13章 画像投稿前まで

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ User.create!(name:  "S",
              activated_at: Time.zone.now)
 
 # 追加のユーザーをまとめて生成する
-99.times do |n|
+30.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
@@ -18,4 +18,11 @@ User.create!(name:  "S",
                password_confirmation: password,
                activated: true,
                activated_at: Time.zone.now)
+end
+
+# ユーザーの一部を対象にマイクロポストを生成する
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(word_count: 5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
